c7d31cfc55617d54933794ba208de2757d5073c2
Sophiamanga11 <117935779+Sophiamanga11@users.noreply.github.com>
2024-08-30 17:40:27 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "Qual tipo de pincel é ideal para aplicar base líquida?",
+        alternativas: [
+            {
+                texto: "A) Pincel kabuki",
+                afirmacao: [
+                    "a)Pincel kabuki"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B)Pincel leque",
+                afirmacao: [
+                    "a) Pincel kabuki"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "O que é 'esfumado' na maquiagem dos olhos?",
+        alternativas: [
+            {
+                texto: "A)Aplicar uma cor de sombra em toda a pálpebra",
+                afirmacao: [
+                    "B)Técnica de aplicar sombra de forma difusa e sem linhas marcadas.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B)Técnica de aplicar sombra de forma difusa e sem linhas marcadas",
+                afirmacao: [
+                    "B)Técnica de aplicar sombra de forma difusa e sem linhas marcadas"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o produto mais indicado para manter o batom por mais tempo nos lábios??",
+        alternativas: [
+            {
+                texto: "A)Primer labial",
+                afirmacao: [
+                    "b) Lip balm"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "b) Lip balm",
+                afirmacao: [
+                    "b) Lip balm."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "O que é contorno na maquiagem?",
+        alternativas: [
+            {
+                texto: "a) Técnica para iluminar o rosto",
+                afirmacao: [
+                    "b) Técnica para realçar e criar sombras no rosto"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "b) Técnica para realçar e criar sombras no rosto",
+                afirmacao: [
+                    "b) Técnica para realçar e criar sombras no rosto"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual desses itens é usado para corrigir manchas e olheiras?",
+        alternativas: [
+            {
+                texto: "A) Corretivo",
+                afirmacao: [
+                    "A)Corretivo"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "B) Base",
+                afirmacao: [
+                    "A)Corretivo"
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a função principal do primer na maquiagem?",
+        alternativas: [
+            {
+                texto: "A) Hidratar o rosto",
+                afirmacao: [
+                    "B)Preparar a pele e melhorar a fixação da maquiagem"
+                ],
+            },
+            {
+                texto: "B)Preparar a pele e melhorar a fixação da maquiagem",
+                afirmacao: [
+                    "B)Preparar a pele e melhorar a fixação da maquiagem."
+                ],
+            },
+        ]
+    },
+];
